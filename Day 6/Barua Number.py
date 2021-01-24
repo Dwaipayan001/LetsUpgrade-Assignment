1465515a760316cffe9c9f9b3650802a58f76fcc
Dwaipayan001 <71640696+Dwaipayan001@users.noreply.github.com>
2021-01-24 18:08:43 +0530

    Add files via upload

---
+++ Day 6/Barua Number.py
@@ -0,0 +1,6 @@
+#letsupgrade Assignment Day 6
+a = int(input("Enter the First Number : "))
+b = int(input("Enter the Second Number : "))
+c = int(input("Enter the Third Number : "))
+d = a*b*c
+print(d)
